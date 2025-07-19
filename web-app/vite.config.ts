a4c4a56a47df5b8919f62f23f299637a3941b7eb
AlexD717 <alex717nd@gmail.com>
2025-07-19 10:25:12 -0700

    Simple Routing + New Logo

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: "/Dont-Press-the-Button/",
+    base: mode === "production" ? "/Board-Game-Turn-Timer/" : "/",
     server: {
         port: 3000,
         open: true,
     },
-})
+}))
